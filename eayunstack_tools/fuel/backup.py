6e9bd4f7e76891fb44a0ea813a89c3bfca7513c7
Coffee <coffee.zyr@gmail.com>
2015-04-01 14:18:05 +0800

    Edit the LOG part

--- eayunstack_tools/fuel/backup.py
+++ eayunstack_tools/fuel/backup.py
@@ -1,14 +1,13 @@
-from utils import set_logger
 from utils import backup_list, backup_new
+import logging
 
 # Use the default DIR to backup
 
-#logger = set_logger()
+LOG = logging.getLogger(__name__)
 
 def backup(parser):
-    logger = set_logger()
     if parser.NEW_BACKUP:
-        new_backup(logger)
+        new_backup()
     if parser.LIST_BACKUP:
         list_backup()
 
@@ -32,19 +31,19 @@ def make(parser):
     )
     parser.set_defaults(func=backup)
 
-def new_backup(logger):
-    logger.info('Starting Backup ...')
-    logger.info('It will take about 30 minutes, Please wait ...')
+def new_backup():
+    LOG.info('Starting Backup ...')
+    LOG.info('It will take about 30 minutes, Please wait ...')
     (stat, out) = backup_new()
     if stat != 0:
         check = """
             * No deployment tasks are currently running.
             * You have at least 11GB free disk space in /var.
         """
-        logger.error('Unexpected Error')
-        logger.error('Please check the information below:\n %s', check)
+        LOG.error('Unexpected Error')
+        LOG.error('Please check the information below:\n %s', check)
     else:
-        logger.info('Backup successfully completed!\n')
+        LOG.info('Backup successfully completed!\n')
         print 'You can use "eayunstack fuel backup [ -l | --list ]" to list your backups\n'
 
 def list_backup():
