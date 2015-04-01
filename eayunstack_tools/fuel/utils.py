e28d6351291ee96320fff525e3e5cc0129031d02
Coffee <coffee.zyr@gmail.com>
2015-04-01 14:30:17 +0800

    delete logger part

--- eayunstack_tools/fuel/utils.py
+++ eayunstack_tools/fuel/utils.py
@@ -1,54 +1,16 @@
-import logging
+# @file utils.py
 import os
 import commands
 import time
 from prettytable import PrettyTable
+import logging
 
-BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+LOG = logging.getLogger(__name__)
 
-COLORS = {
-    'ERROR': RED,
-    'INFO': GREEN
-}
-
-RESET_SEQ = "\033[0m"
-COLOR_SEQ = "\033[1;%dm"
-BOLD_SEQ = "\033[1m"
-
-BASE_COLOR_FORMAT = "[ %(color_levelname)-17s ] %(message)s" 
-
-BACKUP_DIR = '/var/backup/fuel'
+BACKUP_DIR = '/home/coffee/learning'
 
 dir_list = {}
 file_list = {}
-
-def formatter_message(message):
-    message = message.replace("$RESET", RESET_SEQ).replace("$BOLD", BOLD_SEQ)
-    return message
-
-
-class ColoredFormatter(logging.Formatter):
-    def __init__(self, msg):
-        logging.Formatter.__init__(self, msg)
-
-    def format(self, record):
-        levelname = record.levelname
-        if levelname in COLORS:
-            levelname_color = COLOR_SEQ % (30 + COLORS[levelname]) + levelname + RESET_SEQ
-            record.color_levelname = levelname_color
-        return logging.Formatter.format(self, record)
-
-def color_format():
-    color_fmt = formatter_message(BASE_COLOR_FORMAT)
-    return ColoredFormatter(color_fmt)
-
-def set_logger():
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    console = logging.StreamHandler()
-    console.setFormatter(color_format())
-    logger.addHandler(console)
-    return logger
 
 def backup_new():
     (stat, out) = commands.getstatusoutput('dockerctl backup')
@@ -59,6 +21,7 @@ def backup_list():
     backup_dirs.sort(compare)
     i = 1
     t = PrettyTable(['ID', 'Create Time', 'File Name'])
+    # when taking restore, there will be dirs named 'restore'. will not list 'restore' dir.
     not_backup = 'restore'
     for backup_dir in backup_dirs:
         if not_backup in backup_dir:
@@ -79,14 +42,8 @@ def backup_list():
 
 def restore_backup(id):
     backup_list()
-    if isinstance(id, int):
-        if id in file_list.keys():
-            backup_file = BACKUP_DIR + '/' + dir_list[id] + '/' + file_list[id]
-            (stat, out) = commands.getstatusoutput('dockerctl restore %s' % (backup_file))
-        else:
-            print 'The ID does not exist! please retry.'
-    else:
-        print 'Please enter a integer number'
+    backup_file = BACKUP_DIR + '/' + dir_list[id] + '/' + file_list[id]
+    (stat, out) = commands.getstatusoutput('dockerctl restore %s' % (backup_file))
     return (stat, out)
 
 # Sort the file, the file of most recent content modification will located at the end of the table
