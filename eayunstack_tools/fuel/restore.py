09d102b6bb21d5df50faa7756b1a0e04d06503e9
Coffee <coffee.zyr@gmail.com>
2015-04-09 13:35:52 +0800

    move restore part from utils.py to restore.py, rewrite restore method: add -f/--file argument to let user backup from the specified path

--- eayunstack_tools/fuel/restore.py
+++ eayunstack_tools/fuel/restore.py
@@ -1,6 +1,8 @@
-# @file restore.py
 import logging
-import utils
+import os
+import commands
+from utils import BACKUP_DIR
+from utils import read_db
 
 
 LOG = logging.getLogger(__name__)
@@ -8,6 +10,10 @@ LOG = logging.getLogger(__name__)
 def restore(parser):
     if parser.ID:
         restore_bck(parser.ID)
+    elif parser.FILE:
+        restore_file(parser.FILE)
+    else:
+        parser.help
 
 def make(parser):
     '''Fuel Restore'''
@@ -18,30 +24,51 @@ def make(parser):
         dest = 'ID',
         type = int,
         help = 'Specify the ID you want to restore'
-    )
+    )   
+    parser.add_argument(
+        '-f',
+        '--file',
+        action = 'store',
+        dest = 'FILE',
+        help = 'Specify the backup path to restore'
+    )   
     parser.set_defaults(func=restore)
 
-def restore_bck(id):
-    if isinstance(id, int):
-        utils.backup_list()
-        if id in utils.file_list.keys():
-            LOG.info('Starting Restore ...')
-            LOG.info('It will take about 30 minutes, Please wait ...\n')
-            (stat, out) = utils.restore_backup(id)
-            if stat != 0:
-                check = """
-            * The Fuel version is the same release as the backup.
-            * There are no deployments running.
-            * At least 11GB free space in /var.
-                """
-                LOG.error('Unexpected Error')
-                LOG.error('Please check the information below:\n %s', check)
-                print out
-            else:
-                LOG.info('Restore successfully completed!\n')
+
+def restore_id(id):
+    LOG.info('Starting Restore ...')
+    LOG.info('It will take about 30 minutes, Please wait ...\n')
+    backups = read_db()
+    backup_list = {}
+    backup_path = ''
+    for line in backups:
+        line = line.strip('\n')
+        # Put db info into a dictory
+        backup_list[line.split(' ')[0]] = line.split(' ')[1]
+    if id in backup_list.keys():
+        if os.path.isfile(BACKUP_DIR + '/' + backup_list[id]):
+            backup_path = BACKUP_DIR + '/' \
+                          + backup_list[id]
         else:
-            LOG.error('The ID does not exist! please try again.\n')
+            backup_path = BACKUP_DIR + '/' \
+                          + backup_list[id].split('.', 1)[0].split('_', 1)[1] + '/' \
+                          + backup_list[id]
+        (stat, out) = commands.getstatusoutput('dockerctl restore %s' % backup_path)
+        if stat != 0:
+            LOG.error('%s', out)
+        else:
+            LOG.info('Restore successfully completed!\n')
     else:
-        LOG.error('Please enter a integer number.\n')
+        LOG.error('The ID does not exist, Please try again.\n')
+
+
+def restore_file(backup_path):
+    LOG.info('Starting Restore ...')
+    LOG.info('It will take about 30 minutes, Please wait ...\n')
+    (stat, out) = commands.getstatusoutput('dockerctl restore %s' % backup_path)
+    if stat != 0:
+        LOG.error('%s', out)
+    else:
+        LOG.info('Restore successfully completed!\n')
 
 
